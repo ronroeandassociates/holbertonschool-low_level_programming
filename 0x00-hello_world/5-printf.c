fe41295cb19ed4a5f2cd0113f67cc7771fa6d7fb
ronroeandassociates <3321@holbertonschool.com>
2021-06-23 09:37:06 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry Point output a string
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
