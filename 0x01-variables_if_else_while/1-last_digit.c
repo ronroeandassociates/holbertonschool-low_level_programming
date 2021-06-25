47d641660f7660f2151bcc87300136e58ac8d65d
ronroeandassociates <3321@holbertonschool.com>
2021-06-25 14:00:15 -0700

    lst time

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+* main - source code changes to last digit
+* Return:0 (Success)
+*/
 int main(void)
 {
 	int n;
