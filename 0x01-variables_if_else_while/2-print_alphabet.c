45c54f6e3e09e8f706c22b252785bd422549f390
ronroeandassociates <3321@holbertonschool.com>
2021-06-25 16:23:10 -0700

    alpha 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * print out lc letter alpha
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int lowercase = 97;
+	int lastcase = 26;
+		for (i = 0; i < lastcase; i++
+		{
+			putchar(lowercase + i);
+		}
+		putchar('\n');
+		return (0);
+}
