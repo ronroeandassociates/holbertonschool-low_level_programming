664c48be3cb79347c49c13ab6717498974c14979
ronroeandassociates <3321@holbertonschool.com>
2021-06-28 12:55:50 -0700

    alpha 99

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,20 @@
 #include "holberton.h"
 /**
- * print_alphabet - function to print the alphabet
- * Return:0
- */
-int main(void)
-{
-	char a = 'a';
+* print_alphabet_x10 - Print alphabet in lowercase 10 time
+* Return: 0
+*/
+void print_alphabet_x10(void)
+	{
+	int i = 0;
+	char c;
 
-	while (a <= 'z')
-	_putchar(a);
-	a++;
-	Return (0)
-}
+	while (i < 10)
+	{
+	c = 'a';
+	while (c <= 'z')
+	{
+	_putchar(c);
+	_putchar('\n');
+	i++;
+	}
+	}
