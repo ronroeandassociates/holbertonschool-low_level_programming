83905522e6c33335d43cf5cbb3eefc72bf06dac3
ronroeandassociates <3321@holbertonschool.com>
2021-06-28 08:49:21 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - Print Holberton
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char n[9] = "Holberton";
+	for (i = 0; i < 9; i++)
+	{
+	_putchar(n[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
