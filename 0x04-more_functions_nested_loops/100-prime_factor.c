2669f410ae803edb79278efcb88efc1f038ba9dc
ronroeandassociates <3321@holbertonschool.com>
2021-07-01 11:42:51 -0700

    prime it

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,15 +9,20 @@ int main(void)
 	long int num = 612852475143, i = 2;
 
 	while (i <= num)
-	if ((num % i) == 0)
 	{
-	if (num != i)
-	{
-	num = num / i;
-	}
-	else
-	printf("%ld\n", i);
-	i++;
+		if ((num % i) == 0)
+		{
+			if (num != i)
+			{
+				num = num / i;
+			}
+		}
+		else
+		{
+			printf("%ld\n", i);
+		}
+		i++;
+	
+	}	
 	return (0);
-	}
 }
