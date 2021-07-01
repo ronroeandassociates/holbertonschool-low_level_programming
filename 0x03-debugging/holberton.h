66bb4bdcd465627a9469b10f010fa2855eeb4fb0
ronroeandassociates <3321@holbertonschool.com>
2021-07-01 06:45:59 -0700

    clean up

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef holberton.h
+#define holberton.h
 
 #include <stdio.h>
 
@@ -8,4 +8,4 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day)
 
-#endif /* HOLBERTON_H */
+#endif /* holberton.h */
