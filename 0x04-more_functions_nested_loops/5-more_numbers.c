6c1da8d48589ffb1cbf1c5f5484af254644eb6f2
ronroeandassociates <3321@holbertonschool.com>
2021-07-01 10:29:07 -0700

    write it 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,13 @@ void more_numbers(void)
 	int x, c;
 
 	for (x = 0; x < 10; x++)
-	for (c = 0; c <= 14; c++)
-	{
-	if (c > 9)
-	_putchar((c / 10) + '0');
+	{	
+		for (c = 0; c <= 14; c++)
+		{
+		if (c > 9)
+			_putchar((c / 10) + '0');
 	_putchar((c % 10) + '0');
 	}
 	_putchar('\n');
+	}
 }
