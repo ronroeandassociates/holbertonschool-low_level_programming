f7aafb68d447213ad938b5a7e4ec101429e12f39
ronroeandassociates <3321@holbertonschool.com>
2021-07-01 10:57:03 -0700

    sqare itup

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,11 +9,13 @@ void print_square(int size)
 	int i, c;
 
 	if (size > 0)
-	for (i = 0; i < size; i++)
 	{
-	for (c = 0; c < size; c++)
+		for (i = 0; i < size; i++)
+	{
+		for (c = 0; c < size; c++)
 		_putchar('#');
 		_putchar('\n');
+	}
 	}
 	else
 	_putchar('\n');
