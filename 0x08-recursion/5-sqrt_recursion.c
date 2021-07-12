4a28da25e54eb51d969721f0ab0542cad39ef3c5
ronroeandassociates <3321@holbertonschool.com>
2021-07-12 13:27:31 -0700

    sqrt one more time

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,27 @@
 #include "holberton.h"
-
-int actual_sqrt_recursion(int n, int i);
-
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate the square root of
+ * check - checks for the square root
+ * @a:int
+ * @b:int
  *
- * Return: the resulting square root
+ * Return: int
  */
+int check(int a, int b)
+{
+	if (a * a == b)
+	return (a);
+	if (a * a > b)
+	return (-1);
+	return (check(a + 1, b));
+}
+/**
+* _sqrt_recursion - returns the natural square root of a number
+* @n: integer to find sqrt of
+* Return: natural square root or -1
+*/
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	if (n == 0)
+	return (0);
+	return (check(1, n));
 }
