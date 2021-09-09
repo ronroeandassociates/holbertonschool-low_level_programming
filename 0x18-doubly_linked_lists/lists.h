fdd6ad97c022c396f587da0ff0004b382a41146c
ronroeandassociates <3321@holbertonschool.com>
2021-09-09 11:04:03 -0700

    fixed issue

--- 0x18-doubly_linked_lists/lists.h
+++ 0x18-doubly_linked_lists/lists.h
@@ -6,14 +6,14 @@
 #include <string.h>
 
 /**
- *  * struct dlistint_s - doubly linked list
- *   * @n: integer
- *    * @prev: points to the previous node
- *     * @next: points to the next node
- *      *
- *       * Description: doubly linked list node structure
- *        * for Holberton project
- *         */
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
 typedef struct dlistint_s
 {
 		int n;
