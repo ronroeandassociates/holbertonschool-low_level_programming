7881f576c01bba58251658a5730217bb274eae42
Ronroeandassociates <3321@holbertonschool.com>
2022-02-16 13:15:43 -0600

    betty

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -8,19 +8,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new_table = malloc(sizeof(hash_table_t));
-  unsigned long int i;
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
-  if (new_table == NULL)
-    return (NULL);
-  new_table->size = size;
-  new_table->array = malloc(size * sizeof(hash_node_t *));
-  if (new_table->array == NULL)
-  {
-    free(new_table);
-    return (NULL);
-  }
-  for (i = 0; i < size; i++)
-    new_table->array[i] = NULL;
-  return (new_table);
+	if (new_table == NULL)
+		return (NULL);
+	new_table->size = size;
+	new_table->array = malloc(size * sizeof(hash_node_t *));
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+	return (new_table);
 }
