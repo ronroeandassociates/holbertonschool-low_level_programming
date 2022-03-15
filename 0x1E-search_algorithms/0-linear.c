4ce8450f31225da4bd3473cf6708c48c76994ff8
Ronroeandassociates <3321@holbertonschool.com>
2022-03-15 11:24:59 -0500

    Fixed: Betty Style
    changed spaces to Tab to resolve Betty Compliance Issues

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,14 +11,14 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-  int i;
-  int j = size;
+	int i;
+	int j = size;
 
-  for (i = 0; i < j; i++)
-  {
-    printf("Value checked array[%d] = [%d]\n", i, array[i]);
-    if (array[i] == value)
-      return (i);
-  }
-  return (-1);
+	for (i = 0; i < j; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,22 +11,22 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-  int l, r, mid;
+	int l, r, mid;
 
-  for (l = 0, r = size - 1; r >= l;)
-  {
-    printf("Searching in array: ");
-    for (mid = l; mid < r; mid++)
-      printf("%d, ", array[mid]);
-    printf("%d\n", array[mid]);
+	for (l = 0, r = size - 1; r >= l;)
+	{
+		printf("Searching in array: ");
+		for (mid = l; mid < r; mid++)
+			printf("%d, ", array[mid]);
+		printf("%d\n", array[mid]);
 
-    mid = l + (r - l) / 2;
-    if (array[mid] == value)
-      return (mid);
-    if (array[mid] < value)
-      l = mid + 1;
-    else
-      r = mid - 1;
-  }
-  return (-1);
+		mid = l + (r - l) / 2;
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			l = mid + 1;
+		else
+			r = mid - 1;
+	}
+	return (-1);
 }
