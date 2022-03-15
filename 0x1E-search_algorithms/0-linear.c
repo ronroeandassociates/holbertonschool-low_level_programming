2e389e4a0386994386b262cc8678d155b2e0d52e
Ronroeandassociates <3321@holbertonschool.com>
2022-03-15 11:10:44 -0500

    Task: 0. Linear search
    Write a function that searches for a value in an array of integers using the Linear search algorithm
    
    Prototype : int linear_search(int *array, size_t size, int value);
    Where array is a pointer to the first element of the array to search in
    size is the number of elements in array
    And value is the value to search for
    Your function must return the first index where value is located
    If value is not present in array or if array is NULL, your function must return -1
    Every time you compare a value in the array to the value you are searching, you have to print this value

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that searches for a value in an
+ * array of integers using the Linear search algorithm
+ * @array: is a pointer to the first element of the array to search
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ *
+ * Return: the first index where value is located, or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+  int i;
+  int j = size;
+
+  for (i = 0; i < j; i++)
+  {
+    printf("Value checked array[%d] = [%d]\n", i, array[i]);
+    if (array[i] == value)
+      return (i);
+  }
+  return (-1);
+}
