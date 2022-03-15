30e7a7647149b766031ca3a438a1457477a93eb1
Ronroeandassociates <3321@holbertonschool.com>
2022-03-15 11:12:39 -0500

    1. Binary search
    Write a function that searches for a value in a sorted array of integers using the Binary search algorithm
    
    Prototype : int binary_search(int *array, size_t size, int value);
    Where array is a pointer to the first element of the array to search in
    size is the number of elements in array
    And value is the value to search for
    Your function must return the index where value is located
    You can assume that array will be sorted in ascending order
    You can assume that value won’t appear more than once in array
    If value is not present in array or if array is NULL, your function must return -1
    You must print the array being searched every time it changes. (e.g. at the beginning and when you search a subarray)

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - function that searches for a value
+ * in a sorted array of integers using the Binary search algorithm
+ * @array: is a pointer to the first element of the array to search
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ *
+ * Return: return the index where value is located, or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+  int l, r, mid;
+
+  for (l = 0, r = size - 1; r >= l;)
+  {
+    printf("Searching in array: ");
+    for (mid = l; mid < r; mid++)
+      printf("%d, ", array[mid]);
+    printf("%d\n", array[mid]);
+
+    mid = l + (r - l) / 2;
+    if (array[mid] == value)
+      return (mid);
+    if (array[mid] < value)
+      l = mid + 1;
+    else
+      r = mid - 1;
+  }
+  return (-1);
+}
